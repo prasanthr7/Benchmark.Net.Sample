efc2705bebcf58b3de3195045131b23bb1bd4f25
PRASANTH <prasanth7@outlook.com>
2019-06-11 00:24:03 +0530

    Add project files.

---
+++ Benchmark.NET.Sample/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark.NET.Sample
+{
+    class Calculator
+    {
+        public static long Square(long number)
+        {
+            return number * number;
+        } 
+    }
+}
---
+++ Benchmark.NET.Sample/ClaculatorBenchmarkTest.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark.NET.Sample
+{
+    [MinColumn, MaxColumn]
+    [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+    public class ClaculatorBenchmarkTest
+    {
+        [Params(1,2)]
+        public long Number { get; set; }
+
+        [Benchmark]
+        public  long  Square_Performace_Test()
+        {
+            return Calculator.Square(Number);
+        }
+    }
+}
